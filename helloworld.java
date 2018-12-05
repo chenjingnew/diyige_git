c65815b4dbe5cc1499d3f851a49cacd8c89ec814
chenjing <m15886630156@163.com>
2018-12-05 19:28:45 +0800

    修改helloworld 添加一个show方法

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,10 @@
 public class helloworld{
 	
 	public static void main(String[] args){
-		System.out.println("hello world")
+		System.out.println("hello world");
+	}
+
+	public static void show(){
+		System.out.println("showTime");
 	}
 }
